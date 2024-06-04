d51a33b4f0f998042a4c248fd9a2e5428be0d403
meroozcan <mericozcanyedek@gmail.com>
2024-06-04 19:18:43 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+
+@bot.command()
+async def toplama(ctx, sayi1: int, sayi2: int):
+    await ctx.send(sayi1 + sayi2)
+
+bot.run("")
